ead9ca5836e83bf7ef5d14ec42733d1eb1af8e5d
eliasGonzaloB <77998656+eliasGonzaloB@users.noreply.github.com>
2021-03-05 03:57:30 -0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+class LoginView(View):
+    
+    def get(self, request, *args, **kgars):
+        return HttpResponse('LOGIN')
