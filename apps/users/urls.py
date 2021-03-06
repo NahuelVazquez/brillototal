6031d087319d85899aa0c0a8a43b0a75d39b058e
mfl1980 <41476794+mfl1980@users.noreply.github.com>
2021-03-05 23:24:21 -0300

    Nuevas vinculaciones a templates y CSS

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from apps.users.views import LoginView
+from apps.users.views import *
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='users/welcome.html'), name = 'welcome'),
-    path('login/', LoginView.as_view(), name = 'login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('misturnos/', MisTurnosView.as_view(), name='misturnos'),
+    path('registrar/', RegistrarView.as_view(), name='registrar'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,29 +1,59 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.db import transaction
+from apps.users.models import Cliente
 
 
+# Clase para el login
 class LoginView(View):
 
     template_name = "users/login.html"
+    template_mis_turnos = "users/misturnos.html"
+   
     
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
   
     def post(self, request, *args, **kwargs):
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        if user: 
-            return HttpResponse(f'user {user.username}')
-        
+        if user:
+            request.user=user 
+            return HttpResponseRedirect(reverse('misturnos'))
+            
+            
         return render(request, self.template_name, context={'error':'USER Y/O PASSWORD INCORRECTO'})
-        
-   
-   
+           
 
+class RegistrarView(View):
+    
+    template_name = "users/registrar.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        usuario = request.POST.get('usuario')
+        user = User.objects.create_user(username=usuario, email=email, password=password, first_name=nombre, last_name=apellido)
+        telefono = int(request.POST.get('telefono'))
+        cliente = Cliente.objects.create(telefono=telefono, usuario=user)
+        request.user=user
+        return HttpResponseRedirect(reverse('misturnos'))
+        # to do SIEMPRE TIRA USUARIO "MARCO"
 
-
-
-
-
-
+class MisTurnosView(View):
+    
+    template_name = "users/misturnos.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={'user': request.user.username})
+    
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name)
