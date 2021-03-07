6e19fe3b0d67da7eb5bf8c55c98193ccf8161944
NahuelVazquez <martinnahuelvazquez@gmail.com>
2021-03-06 21:03:34 -0300

    redireccionar html y crear extend en html

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,11 +4,12 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='users/welcome.html'), name = 'welcome'),
+    path('', TemplateView.as_view(
+        template_name='users/welcome.html'), name='welcome'),
     path('login/', LoginView.as_view(), name='login'),
     path('misturnos/', MisTurnosView.as_view(), name='misturnos'),
     path('registrar/', RegistrarView.as_view(), name='registrar'),
-    path('solicitarturno/', SolicitarTurnoView.as_view(), name="solcitarturno"),
+    path('solicitarturno/', SolicitarTurnoView.as_view(), name="solicitarturno"),
     path('miperfil/', MiPerfilView.as_view(), name='miperfil'),
     path('modificarturno/', ModificarTurnoView.as_view(), name="modificarturno"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -11,29 +11,29 @@ from apps.users.models import Cliente
 class LoginView(View):
 
     template_name = "users/login.html"
-    
-   
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-  
+
     def post(self, request, *args, **kwargs):
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        user = authenticate(username=request.POST.get(
+            'username'), password=request.POST.get('password'))
         if user:
-            request.user=user 
+            request.user = user
             return HttpResponseRedirect(reverse('misturnos'))
-            
-            
-        return render(request, self.template_name, context={'error':'USER Y/O PASSWORD INCORRECTO'})
-           
-#Clase para el registro del usuario
+
+        return render(request, self.template_name, context={'error': 'USER Y/O PASSWORD INCORRECTO'})
+
+# Clase para el registro del usuario
+
+
 class RegistrarView(View):
-    
+
     template_name = "users/registrar.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
     @transaction.atomic
     def post(self, request, *args, **kwargs):
         nombre = request.POST.get('nombre')
@@ -41,70 +41,77 @@ class RegistrarView(View):
         email = request.POST.get('email')
         password = request.POST.get('password')
         usuario = request.POST.get('usuario')
-        user = User.objects.create_user(username=usuario, email=email, password=password, first_name=nombre, last_name=apellido)
+        user = User.objects.create_user(
+            username=usuario, email=email, password=password, first_name=nombre, last_name=apellido)
         telefono = int(request.POST.get('telefono'))
         cliente = Cliente.objects.create(telefono=telefono, usuario=user)
-        request.user=user
+        request.user = user
         return HttpResponseRedirect(reverse('misturnos'))
-        
-#Clase para visualizar los turnos
+
+# Clase para visualizar los turnos
+
+
 class MisTurnosView(View):
-    
+
     template_name = "users/misturnos.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'user': request.user.username})
         # to do SIEMPRE TIRA USUARIO "MARCO"
-    
+
     def post(self, request, *args, **kwargs):
         return HttpResponseRedirect(reverse('modificarturno'))
 
-#Clase para solicitar un nuevo turno
+# Clase para solicitar un nuevo turno
+
+
 class SolicitarTurnoView(View):
-    
+
     template_name = "users/solicitarturno.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'user': request.user.username})
-    
-    #FALTA DEFINIR VALORES INGRESADOS EN VARIABLES
+
+    # FALTA DEFINIR VALORES INGRESADOS EN VARIABLES
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        vehiculo = request.POST.get('''# Que va acá''' ) 
+        vehiculo = request.POST.get('vehiculo')
         matricula = request.POST.get('matricula')
-        metodo_pago = request.POST.get('''# Que va acá''')
+        metodo_pago = request.POST.get('pago')
         fecha = request.POST.get('fecha')
-        horario = request.POST.get('''# Que va acá''')
+        horario = request.POST.get('horario')
         return HttpResponseRedirect(reverse('solicitarturno'))
 
-#Clase para ver el perfil de usuario   
+# Clase para ver el perfil de usuario
 # falta implementar como se muestran los valores
+
+
 class MiPerfilView(View):
     template_name = "users/miperfil.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'user': request.user.username})
         # to do SIEMPRE TIRA USUARIO "MARCO"
-    
+
     def post(self, request, *args, **kwargs):
         return HttpResponseRedirect(reverse('miperfil'))
 
-#Clase para modificar los turnos
+# Clase para modificar los turnos
+
+
 class ModificarTurnoView(View):
-    
+
     template_name = "users/modificarturno.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'user': request.user.username})
-    
-    #FALTA DEFINIR VALORES INGRESADOS EN VARIABLES
+
+    # FALTA DEFINIR VALORES INGRESADOS EN VARIABLES
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        vehiculo = request.POST.get('''# Que va acá''' ) 
+        vehiculo = request.POST.get('''# Que va acá''')
         matricula = request.POST.get('matricula')
         metodo_pago = request.POST.get('''# Que va acá''')
         fecha = request.POST.get('fecha')
         horario = request.POST.get('''# Que va acá''')
         return HttpResponseRedirect(reverse('modificarturno'))
-    
-    
